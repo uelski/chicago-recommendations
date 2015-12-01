2b9d1f33d6b08d1bdd0a9265276d23ab4da618c0
Sam Vredenburgh <sam.vburgh@gmail.com>
2015-12-01 16:11:16 -0600

    its working

--- controllers/attractions.rb
+++ controllers/attractions.rb
@@ -1,11 +1,32 @@
 class AttractionsController < ApplicationController
 
 get '/' do
+  @attraction = Attraction.all
+  erb :read_attraction
+end
+
+get '/create' do
+  erb :create_attraction
+end
+
+post '/create' do
+  p params
+  @attrac = Attraction.new
+  @attrac.contributor = params[:contributor]
+  @attrac.place = params[:place]
+  @attrac.type_of_place = params[:type_of_place]
+  @attrac.notes = params[:notes]
+  @attrac.save
   erb :index
 end
 
 get '/api' do
   Attraction.all.to_json
+end
+
+get '/api/:id' do
+  puts params
+  return Attraction.find(params[:id]).to_json
 end
 
 end
